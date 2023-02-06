ad5c8829a226d93d58873876681a41db2d140523
Raj Nallapati <upr4@me.com>
2023-02-05 21:09:55 -0500

    Add comments

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ This is an example of how to create a mysql server using docker compose file and
 
 This entire setup (docker and swift connection) is for development purposes and unsafe for production.  
 
-Usage:
- Install docker desktop and swift on your computer.
+How to run the script:  
+ Install docker desktop and swift on your computer.  
  Go to terminal and navigate to where docker-compose file is. Then type:  
  docker compose up -d  
- To run the swift:  
+   
+ To run the swift console script:  
  swift run  
 
  To shut down docker container, run this in the same folder:  
--- Sources/swift-mysqlkit-console/swift_mysqlkit_console.swift
+++ Sources/swift-mysqlkit-console/swift_mysqlkit_console.swift
@@ -17,20 +17,36 @@ public struct swift_mysqlkit_console {
         )
 
         let mysqlSource = MySQLConnectionSource(configuration: configuration)
-
+        // Create an instance of the MultiThreadedEventLoopGroup class, which represents a 
+        // group of event loops that run on multiple threads. The number of threads is determined 
+        // by the System.coreCount property, which returns the number of cores in the system.
         let eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+
+        // ensure that the code inside the block will be executed before the current scope is exited
         defer { try! eventLoopGroup.syncShutdownGracefully() }
 
+        // Create an instance of the EventLoopGroupConnectionPool class, which provides a 
+        // pool of connections to the MySQL database. The MySQLConnectionSource class is used 
+        // to create a source of connections to the database, and it is initialized with the 
+        // configuration instance.
         let pools = EventLoopGroupConnectionPool(
             source: mysqlSource,
             on: eventLoopGroup
         )
 
+        // Create an instance of the EventLoop class, which is an event loop that can run NIO tasks. 
+        // It is obtained from the EventLoopGroup instance by calling its next() method.
         let eventLoop: EventLoop = pools.eventLoopGroup.next()
 
+        // Create an instance of the ConnectionPool class, which represents a pool of connections 
+        // to a MySQL database. It is obtained from the EventLoopGroupConnectionPool instance by 
+        // calling its pool(for:) method, and passing the eventLoop instance as a parameter.
         let pool = pools.pool(for: eventLoop)
 
-
+        // Create an instance of the MySQLDatabase class, which represents a database connection to 
+        // a MySQL database. It is obtained from the ConnectionPool instance by calling 
+        // its database(logger:) method, and passing an instance of Logger as a parameter. 
+        // The label of the logger is set to "MySQL".
         let mysql = pool.database(logger: Logger(label: "MySQL"))
         
         if let rows = try? mysql.simpleQuery("select * from users;").wait() {
@@ -38,12 +54,11 @@ public struct swift_mysqlkit_console {
             for row in rows {
                 print(row)
             }
-            
         } else {
             print("Connection to mysql db failed!")
         }
         
-
+        // Clear out pools before the block ends.
         pools.shutdown() 
     }
 }
